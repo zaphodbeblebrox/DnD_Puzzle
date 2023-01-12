9343cda960a2c397911ca56418cd92e101eeefc8
zaphodbeblebrox <106115863+withrobm@users.noreply.github.com>
2023-01-12 13:59:55 -0800

    Created attempt left label png and implementation

--- frame_sudoku.py
+++ frame_sudoku.py
@@ -12,26 +12,19 @@ class FrameSudoku:
         self.color = color
         self.program_data = program_data
 
-        self.submit = tk.PhotoImage(file = r".\\images\\submit_default.png")
-        self.submit_pressed = tk.PhotoImage(file = r".\\images\\submit_pressed.png")
+        # Import Dial/Button Images
+        self.attempt_img = [tk.PhotoImage(file = r".\\images\\attempt_0.png"),
+                        tk.PhotoImage(file = r".\\images\\attempt_1.png"),
+                        tk.PhotoImage(file = r".\\images\\attempt_2.png"),
+                        tk.PhotoImage(file = r".\\images\\attempt_3.png")]
+        
 
+        # self.submit = tk.PhotoImage(file = r".\\images\\submit_default.png")
+        # self.submit_pressed = tk.PhotoImage(file = r".\\images\\submit_pressed.png")
+        self.submit = tk.PhotoImage(file = r".\\images\\submit_c.png")
+        self.submit_pressed = tk.PhotoImage(file = r".\\images\\submit_uc.png")
         self.dial_img = self.program_data.dial_img_dic_gen()
         
-        # [dial light ON][dial light OFF]
-        # self.dial_light = [[tk.PhotoImage(file = r".\\images\\dial_1_on.png"), tk.PhotoImage(file = r".\\images\\dial_1_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_2_on.png"), tk.PhotoImage(file = r".\\images\\dial_2_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_3_on.png"), tk.PhotoImage(file = r".\\images\\dial_3_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_4_on.png"), tk.PhotoImage(file = r".\\images\\dial_4_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_5_on.png"), tk.PhotoImage(file = r".\\images\\dial_5_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_6_on.png"), tk.PhotoImage(file = r".\\images\\dial_6_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_7_on.png"), tk.PhotoImage(file = r".\\images\\dial_7_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_8_on.png"), tk.PhotoImage(file = r".\\images\\dial_8_off.png")],
-        #                 [tk.PhotoImage(file = r".\\images\\dial_9_on.png"), tk.PhotoImage(file = r".\\images\\dial_9_off.png")]]
-        
-        # for x in range(len(self.dial_light)):
-        #     for y in range(len(self.dial_light[x])):
-        #         self.dial_light[x][y] = self.dial_light[x][y].subsample(2,2)
-
         # Generator Frame Definitions----------
         self.frameGen = tk.LabelFrame(self.root, bd=0, padx=5, pady=5, bg=self.color['background'])
         self.frameGen.grid(row=0, column=0)
@@ -45,24 +38,25 @@ class FrameSudoku:
                 lock_status = 'L' if self.program_data.puzzle_dic["locked"][x][y] == 'x' else 'U'
                 key = light_status + lock_status
 
-                self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_img[self.program_data.puzzle_dic["start"][x][y]][key])
-
-                # if self.program_data.puzzle_dic["locked"][x][y] == 'x':
-                #     self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_light[int(self.program_data.puzzle_dic["start"][x][y])-1][random.randint(0, 1)])
-                # else:
-                #     self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_light[int(self.program_data.puzzle_dic["start"][x][y])-1][random.randint(0, 1)])
+                self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_img[self.program_data.puzzle_dic["start"][x][y]][key], command=lambda id=str(x)+str(y): self.exe_dial_evt(id))
                 self.button_dials[x][y].grid(row=x, column=y, sticky="nsew", padx=5, pady=5)
 
-        self.button_submit = ttk.Button(self.frameGen, image = self.submit)
-        self.button_submit.grid(row=0, column=3, rowspan=3, sticky="nsew", padx=5, pady=5)
+        self.label_attempt = ttk.Label(self.frameGen, image = self.attempt_img[0])
+        self.label_attempt.grid(row=0, column=3, padx=5, pady=5)
+
+        self.button_submit = ttk.Button(self.frameGen, image = self.submit, command = self.check_solution)
+        self.button_submit.grid(row=1, column=3, rowspan=2, sticky="nsew", padx=5, pady=5)
         self.button_submit.bind('<Button-1>', self.button_pressed)
         self.button_submit.bind('<ButtonRelease-1>', self.button_released)
         
-        # example get image path of assigned picture mapped to button
-        # button.cget('image')==str(flag)
-
     def button_pressed(self, event):
         self.button_submit.config(image=self.submit_pressed)
     
     def button_released(self, event):
         self.button_submit.config(image=self.submit)
+
+    def check_solution(self):
+        pass
+    
+    def exe_dial_evt(self, id):
+        pass
