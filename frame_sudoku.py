fae3b1ca5646cf48abb2b0515bbb36ae84c8e7c5
zaphodbeblebrox <106115863+withrobm@users.noreply.github.com>
2023-01-12 15:58:11 -0800

    Buttons can now rotate
    next is changing the lighting of paired dials

--- frame_sudoku.py
+++ frame_sudoku.py
@@ -12,15 +12,14 @@ class FrameSudoku:
         self.color = color
         self.program_data = program_data
 
+        self.locked = 'x'
+
         # Import Dial/Button Images
         self.attempt_img = [tk.PhotoImage(file = r".\\images\\attempt_0.png"),
                         tk.PhotoImage(file = r".\\images\\attempt_1.png"),
                         tk.PhotoImage(file = r".\\images\\attempt_2.png"),
                         tk.PhotoImage(file = r".\\images\\attempt_3.png")]
-        
 
-        # self.submit = tk.PhotoImage(file = r".\\images\\submit_default.png")
-        # self.submit_pressed = tk.PhotoImage(file = r".\\images\\submit_pressed.png")
         self.submit = tk.PhotoImage(file = r".\\images\\submit_c.png")
         self.submit_pressed = tk.PhotoImage(file = r".\\images\\submit_uc.png")
         self.dial_img = self.program_data.dial_img_dic_gen()
@@ -34,11 +33,12 @@ class FrameSudoku:
         self.button_dials = [[0 for i in range(cols)] for j in range(rows)]
         for x in range(0, 3):
             for y in range(0,3):
-                light_status = 'F' if random.randint(0, 1) == 0 else 'N'
-                lock_status = 'L' if self.program_data.puzzle_dic["locked"][x][y] == 'x' else 'U'
-                key = light_status + lock_status
+                key = self.generate_key((x,y))
+                # light_status = 'F' if random.randint(0, 1) == 0 else 'N'
+                # lock_status = 'L' if self.program_data.puzzle_dic["locked"][x][y] == self.locked else 'U'
+                # key = light_status + lock_status
 
-                self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_img[self.program_data.puzzle_dic["start"][x][y]][key], command=lambda id=str(x)+str(y): self.exe_dial_evt(id))
+                self.button_dials[x][y] = ttk.Button(self.frameGen, image = self.dial_img[self.program_data.puzzle_dic["start"][x][y]][key], command=lambda id=(x,y): self.exe_dial_evt(id))
                 self.button_dials[x][y].grid(row=x, column=y, sticky="nsew", padx=5, pady=5)
 
         self.label_attempt = ttk.Label(self.frameGen, image = self.attempt_img[0])
@@ -58,5 +58,29 @@ class FrameSudoku:
     def check_solution(self):
         pass
     
+    def generate_key(self, id):
+        #id is formatted as (x,y) for self.program_data.puzzle_dic[key][x][y] 
+        light_status = 'F' if random.randint(0, 1) == 0 else 'N'
+        lock_status = 'L' if self.program_data.puzzle_dic["locked"][id[0]][id[1]] == self.locked else 'U'
+        return light_status + lock_status
+
     def exe_dial_evt(self, id):
+        #id is formatted as (x,y) for self.program_data.puzzle_dic[key][x][y] 
+        # check if dial is locked
+        if self.program_data.puzzle_dic["locked"][id[0]][id[1]] == self.locked:
+            return
+
+        key = self.generate_key(id)
+        value = int(self.program_data.puzzle_dic["current"][id[0]][id[1]])
+        if value < 4:
+            value = value + 1
+        elif value == 4:
+            value = 1
+        elif value < 8:
+            value = value + 1
+        elif value == 8:
+            value = 5
+        self.program_data.puzzle_dic["current"][id[0]][id[1]] = str(value)
+        self.button_dials[id[0]][id[1]].config(image = self.dial_img[self.program_data.puzzle_dic["current"][id[0]][id[1]]][key])
+
         pass
